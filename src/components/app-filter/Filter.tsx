fea0ed6276ff9a8c9494314d012f798229672c95
kyrisss <kornev.roma62@gmail.com>
2022-06-20 21:30:08 +0300

    feature: add filter

---
+++ src/components/app-filter/Filter.tsx
@@ -0,0 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteFilterTag } from '../../redux/notesSlice';
+import { AppDispatch, RootState } from '../../redux/store';
+
+const Filter = () => {
+
+    const filter = useSelector((state: RootState) => state.notes.filter)
+    const dispatch = useDispatch<AppDispatch>()
+    
+    const onDeleteTag = (e: any) =>{
+        dispatch(deleteFilterTag(e.currentTarget.textContent))
+    }
+
+    const mapFilter = filter.map((tag, i) => {
+        return (
+            <span key={i} className="tag" onClick={onDeleteTag}>{tag}</span>
+        )
+    })
+
+    return (
+        <div className="filter-group">
+            {mapFilter}
+        </div>
+    )
+}
+
+export default Filter;
